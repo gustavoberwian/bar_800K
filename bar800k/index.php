0dc93b94f43ea4b470f052c1f340485bdbc7fe7e
Gustavo Berwian <57967996+gustavoberwian@users.noreply.github.com>
2022-01-26 14:44:45 -0300

    Add files via upload

---
+++ bar800k/index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<?php if(have_posts()){ while(have_posts()) { the_post(); ?>
+    <h1><?php the_title(); ?></h1>
+    <main><?php the_content(); ?></main>
+<?php } } else { ?>
+
+<?php } ?>
+
+<?php get_footer(); ?>
