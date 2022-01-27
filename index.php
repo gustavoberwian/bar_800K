7432c5c3ecca2c26633d96157f314ed23311feec
Gustavo Berwian 800k <gustavo.rberwian@gmail.com>
2022-01-27 07:45:34 -0300

    Teste commit

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <?php if(have_posts()){ while(have_posts()) { the_post(); ?>
     <h1><?php the_title(); ?></h1>
     <main><?php the_content(); ?></main>
-    <h2></h2>
 <?php } } else { ?>
 
 <?php } ?>
